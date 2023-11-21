9bf0876d801cb1af7506ca2292f916b6f6661e1f
r12922015 <r12922015@ntu.edu.tw>
2023-11-21 15:09:57 +0800

    wip: add some helper funcs

--- jpeg_decoder.py
+++ jpeg_decoder.py
@@ -12,7 +12,7 @@ def perror(err):
 
 def debug_print(msg):
     if True:
-        print(msg)
+        print(msg, end="")
 
 def pop_n(jpeg, n):
     words = jpeg[0:n]
@@ -23,50 +23,63 @@ def pop_n(jpeg, n):
 def bytes_to_int(b):
     return int.from_bytes(b, "big")
 
+def split_byte(b):
+    bits = format(b, '08b')
+    a = int(bits[0:4], 2)
+    b = int(bits[4:8], 2)
+    return a, b
+
+def debug_print_table(indent, t, n, m):
+    debug_print(indent + "Table:\n")
+    for i in range(8):
+        debug_print(indent + "  ")
+        for j in range(8):
+            debug_print(f"{t[i][j]:2d} ")
+        debug_print("\n")
+
 # Application0
 def handle_APP(jpeg):
     La = bytes_to_int(pop_n(jpeg, 2))
-    debug_print(f"  La: {La}")
+    debug_print(f"  La: {La}\n")
     pop_n(jpeg, La - 2)
     # we skip APP for now
     return
 
-# Define Quatization Table
 zigzag_order = [(0, 0), (0, 1), (1, 0), (2, 0), (1, 1), (0, 2), (0, 3), (1, 2), (2, 1), (3, 0), (4, 0), (3, 1), (2, 2), (1, 3), (0, 4), (0, 5), (1, 4), (2, 3), (3, 2), (4, 1), (5, 0), (6, 0), (5, 1), (4, 2), (3, 3), (2, 4), (1, 5), (0, 6), (0, 7), (1, 6), (2, 5), (3, 4), (4, 3), (5, 2), (6, 1), (7, 0), (7, 1), (6, 2), (5, 3), (4, 4), (3, 5), (2, 6), (1, 7), (2, 7), (3, 6), (4, 5), (5, 4), (6, 3), (7, 2), (7, 3), (6, 4), (5, 5), (4, 6), (3, 7), (4, 7), (5, 6), (6, 5), (7, 4), (7, 5), (6, 6), (5, 7), (6, 7), (7, 6), (7, 7)]
+# Define Quatization Table
 def handle_DQT(jpeg, DQTs):
     Lq = bytes_to_int(pop_n(jpeg, 2))
-    debug_print(f"  Lq: {Lq}")
+    debug_print(f"  Lq: {Lq}\n")
     n = 2
     while n != Lq:
         PqTq = bytes_to_int(pop_n(jpeg, 1))
         n += 1
-        bits = format(PqTq, '08b')
-        Pq = int(bits[0:4], 2)
-        Tq = int(bits[4:8], 2)
+        Pq, Tq = split_byte(PqTq)
         Qk = 8 if Pq == 0 else 16
-        debug_print(f"  Pq: {Pq}")
-        debug_print(f"  Tq: {Tq}")
-        debug_print(f"  Qk: {Qk}")
+        debug_print(f"  Pq: {Pq}\n")
+        debug_print(f"  Tq: {Tq}\n")
+        debug_print(f"  Qk: {Qk}\n")
         tmp = [[0 for _ in range(8)] for __ in range(8)]
         for z in range(64):
             i, j = zigzag_order[z]
             tmp[i][j] = (bytes_to_int(pop_n(jpeg, Qk//8)))
-        debug_print(f"  In zigzag: {tmp}")
+        debug_print_table("    ", tmp, 8, 8)
         DQTs[Tq] = tmp
         n += (Qk // 8) * 8 * 8
     return
 
 def handle_DHP(jpeg):
     Lf = bytes_to_int(pop_n(jpeg, 2))
-    P = bytes_to_int(pop_n(jpeg, 1))
-    Y = bytes_to_int(pop_n(jpeg, 2))
-    X = bytes_to_int(pop_n(jpeg, 2))
+    P  = bytes_to_int(pop_n(jpeg, 1))
+    Y  = bytes_to_int(pop_n(jpeg, 2))
+    X  = bytes_to_int(pop_n(jpeg, 2))
     Nf = bytes_to_int(pop_n(jpeg, 1))
-    debug_print(f"  Lf: {Lf}")
-    debug_print(f"  P: {P}")
-    debug_print(f"  Y: {Y}")
-    debug_print(f"  X: {X}")
-    debug_print(f"  Nf: {Nf}")
+    debug_print(f"  Lf: {Lf}\n")
+    debug_print(f"  P: {P}\n")
+    debug_print(f"  Y: {Y}\n")
+    debug_print(f"  X: {X}\n")
+    debug_print(f"  Nf: {Nf}\n")
+    #for _ in range(Nf):
     return
 
 if len(sys.argv) != 2:
@@ -81,13 +94,13 @@ DQTs = {}
 while True:
     wd = pop_n(jpeg, 2)
     if wd == APP:
-        debug_print("APP")
+        debug_print("APP\n")
         handle_APP(jpeg)
     elif wd == DQT:
-        debug_print("DQT")
+        debug_print("DQT\n")
         handle_DQT(jpeg, DQTs)
     elif wd == DHP:
-        debug_print("DHP")
+        debug_print("DHP\n")
         handle_DHP(jpeg)
     else:
         print("X")
